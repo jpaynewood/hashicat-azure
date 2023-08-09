58c58e629af03edc984a61be72ed6aa7ce34caa8
jpaynewood <jpayne@woodforest.com>
2023-08-09 19:54:02 +0000

    Added network module

--- network.tf
+++ network.tf
@@ -1,5 +1,9 @@
 module "network" {
-  source  = "app.terraform.io/JPayne-training/network/azurerm"
-  version = "5.3.0"
-  resource_group_name=azurerm_resource_group.myresourcegroup.name
+  source              = "app.terraform.io/JPayne-training/network/azurerm"
+  version             = "3.5.0"
+  resource_group_name = azurerm_resource_group.myresourcegroup.name
+  tags = {
+    Department = "Billable"
+    Billable   = "True"
+  }
 }
